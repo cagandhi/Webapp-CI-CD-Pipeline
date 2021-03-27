f91e7300320613d3166cf8c22398e6969290249e
Chintan Gandhi <cagandhi@ncsu.edu>
2021-03-26 23:58:08 -0400

    add playbook to store github user, pwd to jenkins cred manager, closes #25

--- commands/setup.js
+++ commands/setup.js
@@ -47,25 +47,25 @@ async function run(privateKey, gh_user, gh_pass) {
     let result = child.spawnSync(`bakerx`, `run config-srv focal --ip 192.168.33.20 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
-    console.log(chalk.blueBright('Running init script...'));
-    result = sshSync('/bakerx/cm/server-init.sh', 'vagrant@192.168.33.20');
-    if( result.error ) { console.log(result.error); process.exit( result.status ); }
-
-    // if either of gh_user and gh_pass value is present
+    // if either of gh_user and gh_pass value is undefined but not both
     if(gh_user || gh_pass) {
 
-        // if both gh_user and gh_pass is present, run the credential storing shell file
+        // if both gh_user and gh_pass are not undefined, execute "store the git credentials" shell file
         if(gh_user && gh_pass) {
             console.log(chalk.blueBright('Storing git credentials ...'));
             result = sshSync(`/bakerx/cm/add-git-cred.sh ${gh_user} ${gh_pass}`, 'vagrant@192.168.33.20');
             if( result.error ) { console.log(result.error); process.exit( result.status ); }
         }
-        // if either of gh_user or gh_pass is not passed in CLI, display error and exit
+        // if one of gh_user or gh_pass is passed but not both in the setup command, display error and exit
         else {
             console.log(chalk.red("Pass both github user and github password as CLI arguments"));
             process.exit(1);
         }
     }
+
+    console.log(chalk.blueBright('Running init script...'));
+    result = sshSync('/bakerx/cm/server-init.sh', 'vagrant@192.168.33.20');
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Setting up the Jenkins server and build environment ...'));
     result = sshSync('/bakerx/cm/run-ansible.sh /bakerx/cm/playbook.yml /bakerx/cm/inventory.ini /bakerx/.vault-pass', 'vagrant@192.168.33.20');
