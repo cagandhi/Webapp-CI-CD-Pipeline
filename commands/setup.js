ca29e4e188b9c253b789c52948d46901ecafc71a
rajshrivastava <rkshriva@ncsu.edu>
2021-03-29 22:56:04 -0400

    Increased vm memory to 4096 MB

--- commands/setup.js
+++ commands/setup.js
@@ -44,7 +44,7 @@ async function run(privateKey, gh_user, gh_pass) {
     console.log(chalk.greenBright('Installing configuration server!'));
 
     console.log(chalk.blueBright('Provisioning configuration server...'));
-    let result = child.spawnSync(`bakerx`, `run config-srv focal --ip 192.168.33.20 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
+    let result = child.spawnSync(`bakerx`, `run config-srv focal --ip 192.168.33.20 --sync --memory 4096`.split(' '), {shell:true, stdio: 'inherit'} );
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     // if either of gh_user and gh_pass value is undefined but not both
