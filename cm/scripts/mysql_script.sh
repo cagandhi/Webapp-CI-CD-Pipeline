11e324f3af06b3be42b0521f621789a7176c3389
Chintan Gandhi <cagandhi@ncsu.edu>
2021-04-01 14:18:30 -0400

    modify cleanup commands and add mysql cleanup shell script

--- cm/scripts/mysql_script.sh
+++ cm/scripts/mysql_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mysql -uUSERNAME -pPASSWORD -hHOSTNAME -P3306 <<EOF
-DROP DATABASE IF EXISTS iTrust2_test
+# https://stackoverflow.com/a/56257417/6543250
+sudo mysql -uroot -proot -P3306 <<EOF
+DROP DATABASE IF EXISTS temp_db
 EOF
