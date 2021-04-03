2ac1943a829d1dcb67852a2f88060b1f811a1c03
Chintan Gandhi <cagandhi@ncsu.edu>
2021-04-02 22:54:51 -0400

    add my.cnf location and modify cleanup script

--- cm/scripts/cleanup.sh
+++ cm/scripts/cleanup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Trace commands as we run them:
+set -x
+
 # https://stackoverflow.com/a/56257417/6543250
-sudo mysql -uroot -proot -P3306 <<EOF
+mysql --defaults-file=/etc/my.cnf <<EOF
 DROP DATABASE IF EXISTS iTrust2_test
 EOF
-sudo kill -9 $(lsof -t -i:8080) || true
-sudo kill -9 $(lsof -t -i:9001) || true
-sudo kill $(pgrep chrome) || true
-sudo kill -9 $(lsof -t -i:9436) || true
+
+pid=$(sudo lsof -t -i:9001)
+sudo kill -9 $pid | true
+
+pid=$(sudo lsof -t -i:8080)
+sudo kill -9 $pid | true
+
+pid=$(sudo pgrep chrome)
+sudo kill -9 $pid | true
