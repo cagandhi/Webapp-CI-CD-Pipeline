a3c64f857d2228689ce1b95601d5054733f85c86
Chintan Gandhi <cagandhi@ncsu.edu>
2021-04-26 15:40:42 -0400

    add files to deploy itrust

--- commands/deploy.js
+++ commands/deploy.js
@@ -13,7 +13,7 @@ exports.builder = yargs => {
         inventory: {
             alias: 'i',
             describe: 'inventory file',
-            default: '/bakerx/provision/inventory.ini',
+            default: '/bakerx/deploy/inventory.ini',
             type: 'string'
         },
     });
@@ -28,49 +28,6 @@ exports.handler = async argv => {
     })();
 
 };
-
-// --------------------------------------------------------------
-
-async function getBuildStatus(job, id) {
-    return new Promise(async function(resolve, reject)
-    {
-        console.log(`Fetching ${job}: ${id}`);
-        let result = await jenkins.build.get(job, id);
-        resolve(result);
-    });
-}
-
-async function waitOnQueue(id) {
-    return new Promise(function(resolve, reject)
-    {
-        jenkins.queue.item(id, function(err, item) {
-            if (err) throw err;
-            // console.log('queue', item);
-            if (item.executable) {
-                console.log('number:', item.executable.number);
-                resolve(item.executable.number);
-            } else if (item.cancelled) {
-                console.log('cancelled');
-                reject('canceled');
-            } else {
-                setTimeout(async function() {
-                    resolve(await waitOnQueue(id));
-                }, 5000);
-            }
-        });
-    });
-  }
-
-
-async function triggerBuild(job)
-{
-    let queueId = await jenkins.job.build(job);
-    let buildId = await waitOnQueue(queueId);
-    return buildId;
-}
-
-//
-// --------------------------------------------------------------
 
 async function run(job_name, inventory) {
 
@@ -88,25 +45,4 @@ async function run(job_name, inventory) {
     let result = sshSync(`/bakerx/cm/run-ansible.sh ${filePath} ${inventoryPath} ${vaultFilePath}`, 'vagrant@192.168.33.20');
     if( result.error ) { process.exit( result.status ); }
 
-    // console.log('Triggering build.')
-    // let buildId = await triggerBuild(`${job_name}`).catch( e => console.log(e));
-
-    // console.log(`Received ${buildId}`);
-    // let build = await getBuildStatus(`${job_name}`, buildId);
-    // console.log( `Build result: ${build.result}` );
-
-    // console.log(`Build output`);
-    // var log = await jenkins.build.logStream({name: `${job_name}`, number: buildId});
-
-    // log.on('data', function(text) {
-    //   process.stdout.write(text);
-    // });
-
-    // log.on('error', function(err) {
-    //   console.log('Error: ', err);
-    // });
-
-    // log.on('end', function() {
-    //   console.log('--- end of pipeline execution ---');
-    // });
 }
