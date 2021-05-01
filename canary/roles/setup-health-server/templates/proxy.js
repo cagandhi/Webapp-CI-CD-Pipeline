8e304843f4b3558ff5f53be1308ab2da629fb18a
Devarsh Shah <dshah3@ncsu.edu>
2021-05-01 12:09:46 -0400

    Updated proxy.js file

--- canary/roles/setup-health-server/templates/proxy.js
+++ canary/roles/setup-health-server/templates/proxy.js
@@ -1,37 +1,45 @@
 const chalk = require('chalk');
 const path = require('path');
 const os = require('os');
-
+const mwu = require('mann-whitney-utest');
 const got = require('got');
 const http = require('http');
 const httpProxy = require('http-proxy');
 
-exports.command = 'serve';
-exports.desc = 'Run traffic proxy.';
-exports.builder = yargs => {};
 
-exports.handler = async argv => {
-    const { } = argv;
-
-    (async () => {
-
-        await run( );
-
-    })();
-
-};
 
 const BLUE  = 'http://192.168.44.25:3000';
 const GREEN = 'http://192.168.44.30:3000';
+
+const blue_preview=`http://192.168.44.25:3000:(service_port)/preview`;
+const green_preview=`http://192.168.44.30:3000:(service_port)/preview`;
 
 class Production
 {
     constructor()
     {
         this.TARGET = GREEN;
+        this.server_id=0;
         setInterval( this.healthCheck.bind(this), 5000 );
-    }
+    
+    //https://stackoverflow.com/questions/6893130/how-to-set-one-minute-counter-in-javascript
+    let minutes=1;
 
+    setTimeout(() => {
+            
+            this.TARGET = BLUE;
+            this.server_id= 1;
+            
+        }, minutes*60*1000);
+        
+        
+    setTimeout(() => {
+        
+        this.TARGET = GREEN;
+        this.server_id = 0;      
+        }, 2*minutes*60*1000);
+    }
+    
     // TASK 1:
     proxy()
     {
@@ -73,7 +81,35 @@ class Production
 async function run() {
 
     console.log(chalk.keyword('pink')('Starting proxy on localhost:3090'));
+    let client = redis.createClient(6379, 'localhost', {});
 
+
+    var latency = setInterval( function ()
+    {
+        for( var server of servers )
+        {
+            if( server.url )
+            {
+                let now = Date.now();
+
+                // Bind a new variable in order to for it to be properly captured inside closure.
+                let captureServer = server;
+
+                // Make request to server we are monitoring.
+                got(server.url, {timeout: 5000, throwHttpErrors: false}).then(function(res)
+                {
+                    // TASK 2
+                    captureServer.statusCode = res.statusCode;
+                    captureServer.latency = Date.now()-now;
+                }).catch( e =>
+                {
+                    // console.log(e);
+                    captureServer.statusCode = e.code;
+                    captureServer.latency = 5000;
+                });
+            }
+        }
+    }, 10000);
     let prod = new Production();
     prod.proxy();
 
