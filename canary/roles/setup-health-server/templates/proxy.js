3f70ff270f132bd7ec23f34ef47784eef7e7421f
Chintan Gandhi <cagandhi@ncsu.edu>
2021-05-01 17:51:20 -0400

    modify tasks agent and server

--- canary/roles/setup-health-server/templates/proxy.js
+++ canary/roles/setup-health-server/templates/proxy.js
@@ -1,11 +1,11 @@
 const chalk = require('chalk');
 const path = require('path');
 const os = require('os');
-const mwu = require('mann-whitney-utest');
+//const mwu = require('mann-whitney-utest');
 const got = require('got');
 const http = require('http');
 const httpProxy = require('http-proxy');
-
+const execSync = require('child_process').execSync;
 
 
 const BLUE  = 'http://192.168.44.25:3000/preview';
@@ -13,6 +13,7 @@ const GREEN = 'http://192.168.44.30:3000/preview';
 
 //const blue_preview=`http://192.168.44.25:3000/preview`;
 //const green_preview=`http://192.168.44.30:3000/preview`;
+
 
 class Production
 {
@@ -38,8 +39,9 @@ class Production
         this.TARGET = GREEN;
         this.server_id = 0;      
         }, 2*minutes*60*1000);
+        */
     }
-    */
+    
     
     // TASK 1:
     proxy()
@@ -60,13 +62,13 @@ class Production
    {
       this.TARGET = GREEN;
    }
-   /*
+   
 
    async healthCheck()
    {
       try
       {
-         const response = await got(this.TARGET, {throwHttpErrors: false});
+         const response = await got(this.TARGET, {throwHttpErrors: false}); // TODO: convert this to post request and add survey.json in payload
          let status = response.statusCode == 200 ? chalk.green(response.statusCode) : chalk.red(response.statusCode);
          console.log( chalk`{grey Health check on ${this.TARGET}}: ${status}`);
 
@@ -77,16 +79,52 @@ class Production
          console.log(error);
       }
    }
-   */
+   
 
 }
 
-async function run() {
+(async () => 
+{
+    // Get agent name from command line.
+    
+    await main();
 
-    console.log(chalk.keyword('pink')('Starting proxy on localhost:3090'));
-    let client = redis.createClient(6379, 'localhost', {});
-    let prod = new Production();
-    prod.proxy();
+})();
+
+async function main() {
+
+  console.log(chalk.keyword('pink')('Starting proxy on localhost:3090'));
+//  let client = redis.createClient(6379, 'localhost', {});
+  let prod = new Production();
+  prod.proxy();
+
+  // send loads to blue and green VMs
+  let start_time = (new Date()).getTime()/1000;
+  while(((new Date()).getTime()/1000) - start_time <= 60)
+  {
+    let response = await got.post("https://api.digitalocean.com/v2/droplets", 
+    {
+      headers:headers,
+      json: data
+    }).catch( err => 
+      console.error(chalk.red(`createDroplet: ${err}`)) 
+    );
+  }
+
+  console.log('siege blue');
+  // import { execSync } from 'child_process';  // replace ^ if using ES modules
+  let cmd = "siege -b -t60s --content-type 'application/json' '"+prod.TARGET+" POST < survey.json'";
+  console.log(cmd);
+  let output = execSync(cmd, { cwd: '/home/vagrant/server/', encoding: 'utf-8', stdio: ['inherit', 'inherit', 'inherit'] });
+
+  prod.TARGET = GREEN;
+
+  console.log("prod target changed now :: "+prod.TARGET);
+  console.log('siege green');
+
+  cmd = "siege -b -t60s --content-type 'application/json' '"+prod.TARGET+" POST < survey.json'";
+  console.log(cmd);
+  output = execSync(cmd, { cwd: '/home/vagrant/server/', encoding: 'utf-8', stdio: ['inherit', 'inherit', 'inherit'] });
 
     /*
     var latency = setInterval( function ()
