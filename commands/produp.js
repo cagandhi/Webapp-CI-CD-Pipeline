246c69c053001a4c58c8cfa668d24dc336282fbb
Raj Kumar Shrivastava <rksmail20@gmail.com>
2021-05-04 22:58:58 -0400

    display output inventory in produp

--- commands/produp.js
+++ commands/produp.js
@@ -30,4 +30,16 @@ async function run() {
     console.log(chalk.blueBright('Provisioning cloud instances...'));
     let result = sshSync(`/bakerx/cm/run-ansible.sh ${filePath} ${inventoryPath} ${vaultFilePath}`, 'vagrant@192.168.33.20');
     if( result.error ) { process.exit( result.status ); }
+
+    console.log(chalk.yellow("\nPrinting output inventory.ini for cloud instances ...\n"));
+    let invFile = path.join(__dirname.split(path.sep).slice(0,-1).join(path.sep), 'inventory.ini');
+    try {
+        const data = fs.readFileSync(invFile, 'UTF-8');
+        const lines = data.split(/\r?\n/);
+        lines.forEach( (line) => {
+            console.log(line);
+        });
+    } catch(err) {
+        console.error(err);
+    }
 }
